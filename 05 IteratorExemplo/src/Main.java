dec316acea4917bbf08427f494aea0faa7726663
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-03-04 13:13:41 -0300

    Created the class Cliente

--- 05 IteratorExemplo/src/Main.java
+++ 05 IteratorExemplo/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Cliente> clientes = new ArrayList<>();
+        clientes.add(new Cliente(3L,"Jão",22));
+        clientes.add(new Cliente(1L,"Maria", 16));
+        clientes.add(new Cliente(2L,"Ana", 17));
+
+        for (Cliente clie : clientes) {
+            System.out.println(clie);
+        }
+
+        System.out.println("-----------------------------------");
+//        Iterator<Cliente> clienteIterator = clientes.iterator(); // forma comum
+//        while (clienteIterator.hasNext()){
+//            if (clienteIterator.next().getIdade() <= 17){
+//                clienteIterator.remove();
+//            }
+//        }
+
+        clientes.removeIf(cliente -> cliente.getId() <= 17); //Forma mais simples: Programação funcional
+        for (Cliente clie : clientes) {
+            System.out.println(clie);
+        }
     }
 }
